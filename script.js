601bd74a1f3d4c5e33fe0532e110ed8de4c86646
Shashank4075 <153177831+Shashank4075@users.noreply.github.com>
2024-11-14 13:33:37 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const coll = document.querySelectorAll(".collapsible");
+
+    coll.forEach(button => {
+        button.addEventListener("click", function() {
+            this.classList.toggle("active");
+            const content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
+    });
+});
